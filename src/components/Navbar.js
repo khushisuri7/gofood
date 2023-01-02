079f65c937453802ff3ddae0c14aba6158cf2cd4
khushi <khushisuri9252gmail.com>
2023-01-02 21:16:24 +0530

    All Screens and components made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import { useCart } from './ContextReducer';
+import Modal from '../Modal';
+import Cart from '../screens/Cart';
+export default function Navbar(props) {
 
-export default function Navbar() {
-  return (
-    <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
-  )
+    const [cartView, setCartView] = useState(false)
+    localStorage.setItem('temp', "first")
+    let navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+
+        navigate("/login")
+    }
+
+    const loadCart = () => {
+        setCartView(true)
+    }
+
+    const items = useCart();
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
+                style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
+                <div className="container-fluid">
+                    <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link fs-5 mx-3 active" aria-current="page" to="/">Home</Link>  {/* index.css - nav-link color white */}
+                            </li>
+                            {(localStorage.getItem("token")) ?
+                                <li className="nav-item">
+                                    <Link className="nav-link fs-5 mx-3 active" aria-current="page" to="/myorder" >My Orders</Link>  {/* index.css - nav-link color white */}
+                                </li> : ""}
+                        </ul>
+                        {(!localStorage.getItem("token")) ?
+                            <form className="d-flex">
+                                <Link className="btn bg-white text-success mx-1 " to="/login">Login</Link>
+                                <Link className="btn bg-white text-success mx-1" to="/signup">Signup</Link>
+                            </form> :
+                            <div>
+
+                                <div className="btn bg-white text-success mx-2 " onClick={loadCart}>
+                                    <Badge color="secondary" badgeContent={items.length} >
+                                        <ShoppingCartIcon />
+                                    </Badge>
+                                    Cart
+                                </div>
+
+                                {cartView ? <Modal onClose={() => setCartView(false)}><Cart></Cart></Modal> : ""}
+
+                                <button onClick={handleLogout} className="btn bg-white text-success" >Logout</button></div>}
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
